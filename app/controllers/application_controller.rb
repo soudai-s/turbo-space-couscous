e2a10e7f338e626d0ad09da61030c2326dd9fef5
Soudai Sasada <4921350+soudai-s@users.noreply.github.com>
2026-05-20 08:34:15 +0000

    Day 6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   include Authentication
+
+  around_action :switch_locale
+
+  private
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 end
